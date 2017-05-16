9b429a039b8551e2cbe22a30710540e39c4de5b7
taylor <taylor.h.dbm@gmail.com>
2017-05-16 22:35:41 +0800

    sets

--- deriv1.scm
+++ deriv1.scm
@@ -5,6 +5,41 @@
 
 (define (exponent p) (caddr p))
 
+(define (make-exponentiation base exponent)
+	(cond ((= exponent 0)
+			1)
+		  ((= exponent 1)
+			base)
+		  (else 
+			(list '** base exponent))))
+
 
 (define (deriv1 exp var)
+	(cond ((number? exp)
+			0)
+		  ((variable? exp)
+			(if (same-variable? exp var)
+				1
+				0))
+		  ((sum? exp)
+			(make-sum (deriv1 (addend exp) var)
+					   (deriv1 (augend exp) var)))
+		  ((product? exp)
+			(make-sum
+				(make-product (multiplier exp)
+							  (deriv1 (multiplicand exp) var))
+				(make-product  (deriv1 (multiplier exp) var)
+								(multiplicand exp))))
+			((exponentiation? exp)
+				(let ((n (exponent exp))
+					   (u (base exp)))
+					 (make-product
+						n
+						(make-product
+							(make-exponentiation
+								u
+								(- n 1))
+							(deriv1 u var)))))
+		(else
+			(error "unknown expression type -- DERIV" exp))))
 	
